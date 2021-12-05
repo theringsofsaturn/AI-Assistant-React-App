c587aed57b582a6fcd6a8d117876bade8643e6d4
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-05 17:15:40 +0100

    useEffect and UseEffect for connecting with AI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 const alanKey =
   "20e4a9f477a935a34175767f24ec24792e956eca572e1d8b807a3e2338fdd0dc/stage";
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command }) => {
-        if (command === "testCommand") {
-          alert("Ky eshte nje testim pr komanden!");
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadLines") {
+          // alert("Ky eshte nje testim pr komanden!");
+          setNewsArticles(articles);
         }
       },
     });
@@ -16,7 +19,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>TEST PER AI</h1>
     </div>
   );
 };
