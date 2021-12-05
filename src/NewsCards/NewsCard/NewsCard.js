58ec28727cbb3902aa6f556bfa74a9ec78f43f0a
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-05 20:41:50 +0100

    card structure

--- src/NewsCards/NewsCard/NewsCard.js
+++ src/NewsCards/NewsCard/NewsCard.js
@@ -1,11 +1,44 @@
 import React from "react";
-import { Grid, Grow, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
 const NewsCard = () => {
   return (
-    <Grow>
-      <NewsCard />
-    </Grow>
+    <Card>
+      <CardActionArea>
+        <CardMedia />
+        <div>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="h2"
+          ></Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="h2"
+          ></Typography>
+        </div>
+        <Typography gutterBottom variant="h5"></Typography>
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          ></Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions></CardActions>
+      <Button size="small" color="primary"></Button>
+      <Typography variant="h5" color="textSecondary"></Typography>
+    </Card>
   );
 };
 
