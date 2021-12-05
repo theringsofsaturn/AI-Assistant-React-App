d289a0f329e0de034d8529391862574e4dfc16e1
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-05 01:51:36 +0100

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'; 
-
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+const alanKey =
+  "20e4a9f477a935a34175767f24ec24792e956eca572e1d8b807a3e2338fdd0dc/stage";
 const App = () => {
-    return (
-        <div>
-        <h1>Hello World</h1>
-        </div>
-    );
-    }
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("Ky eshte nje testim pr komanden!");
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
