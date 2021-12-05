24e054c789ca2f37cc2cc4c8c859a000c469a3c2
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-05 19:48:55 +0100

    applied styles to material ui cards

--- src/NewsCards/NewsCards.js
+++ src/NewsCards/NewsCards.js
@@ -1,15 +1,18 @@
 import { Grid, Grow } from "@material-ui/core";
 import React from "react";
-import NewsCard from "../NewsCard/NewsCard";
+import NewsCard from "../NewsCards/NewsCard/NewsCard";
+import useStyles from './styles'
 
 // getting "articles" from the props in App.js
 const NewsCards = ({ articles }) => {
+  const classes = useStyles();
+
   return (
     <Grow in>
-      <Grid container alignItems="stretch">
+      <Grid classname={classes.container} container alignItems="stretch">
         {articles.map((article, i) => {
           <Grid item xs={12} sm={6} lg={3} style={{ display: "flex" }}>
-            <NewsCard article={article} i={i}/>;
+            <NewsCard article={article} i={i} />;
           </Grid>;
         })}
       </Grid>
