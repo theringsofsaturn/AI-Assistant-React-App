8e44d5391853cb45a0fc341610b284cb73785ef3
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-06 01:44:57 +0100

    commits

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import NewsCards from "./NewsCards/NewsCards";
-const alanKey =
-  "20e4a9f477a935a34175767f24ec24792e956eca572e1d8b807a3e2338fdd0dc/stage";
+import wordsToNumbers from "words-to-numbers";
+import { Typography } from "@material-ui/core";
+import { NewsCards, Modal } from "./components";
+import useStyles from "./styles";
+import logoProfile from "./images/me.jpg";
+import ishmaelLogo from "./images/ishmael.jpeg";
+
 const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
   const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
 
   useEffect(() => {
     alanBtn({
-      key: alanKey,
-      onCommand: ({ command, articles }) => {
-        if (command === "newHeadLines") {
-          // alert("Ky eshte nje testim pr komanden!");
+      key: "20e4a9f477a935a34175767f24ec24792e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
         }
       },
     });
@@ -20,9 +47,56 @@ const App = () => {
 
   return (
     <div>
-      <h1>TEST PER AI</h1>
-      {/* Passing newsArcticles to NewsCards as a prop */}
-      <NewsCards articles={newsArticles} />
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img src={ishmaelLogo} className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            <a
+              className={classes.link}
+              href="https://github.com/theringsofsaturn"
+            >
+              {" "}
+              Emilian Kasemi
+            </a>{" "}
+            -
+            <a
+              className={classes.link}
+              href="https://github.com/theringsofsaturn"
+            >
+              {" "}
+              GitHub
+            </a>
+          </Typography>
+          <img
+            className={classes.image}
+            src={logoProfile}
+            height="50px"
+            alt="AI logo"
+          />
+        </div>
+      ) : null}
     </div>
   );
 };
